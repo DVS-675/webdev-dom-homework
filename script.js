3345399b1dc1c690cdd0a688573042fc638af1be
DVS-675 <dmitryvs20170@gmail.com>
2023-04-03 11:31:12 +0500

    commit

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const buttonElement = document.getElementById("add-button");
-const deleteButtonElement = document.getElementById("delete-button");
-const listElement = document.getElementById("list");
-const nameInputElement = document.getElementById("name-input");
-const textInputElement = document.getElementById("text-input");
-const mainForm = document.querySelector(".add-form");
-const addCommentText = document.getElementById("addingComment");
-const waitCommentMessage = document.getElementById("waitComment");
+import {
+  changeLikesListener,
+  validateInput,
+  addCommentWithEnter,
+  editComment,
+  buttonBlock,
+  deleteComment,
+  renderComments,
+} from "./formActions.js";
+
+export const buttonElement = document.getElementById("add-button");
+export const deleteButtonElement = document.getElementById("delete-button");
+export const listElement = document.getElementById("list");
+export const nameInputElement = document.getElementById("name-input");
+export const textInputElement = document.getElementById("text-input");
+export const mainForm = document.querySelector(".add-form");
+export const addCommentText = document.getElementById("addingComment");
+export const waitCommentMessage = document.getElementById("waitComment");
+export let comments = [];
+
 //GET запрос с сервера
 waitCommentMessage.style.display = "inline";
 const fetchGetAndRender = () => {
@@ -57,28 +69,6 @@ const fetchGetAndRender = () => {
       }
       console.warn(error);
     });
-};
-
-// Изменение лайков
-
-const changeLikesListener = () => {
-  const buttonLikeElements = document.querySelectorAll(".like-button");
-
-  for (const buttonLikeElement of buttonLikeElements) {
-    buttonLikeElement.addEventListener("click", (event) => {
-      event.stopPropagation();
-      const index = buttonLikeElement.dataset.index;
-
-      if (comments[index].isLiked === false) {
-        comments[index].isLiked = true;
-        comments[index].likes += 1;
-      } else if (comments[index].isLiked === true) {
-        comments[index].isLiked = false;
-        comments[index].likes -= 1;
-      }
-      renderComments();
-    });
-  }
 };
 
 //Добавление комментария, POST запрос с GET
@@ -144,95 +134,12 @@ buttonElement.addEventListener("click", () => {
   renderComments();
 });
 
-// блокировка кнопки
-const validateInput = () => {
-  if (nameInputElement.value === "" || textInputElement.value === "") {
-    buttonElement.disabled = true;
-  } else {
-    buttonElement.disabled = false;
-  }
-};
-const buttonBlock = () => {
-  validateInput();
-  document.querySelectorAll("#name-input,#text-input").forEach((el) => {
-    el.addEventListener("input", () => {
-      validateInput();
-    });
-  });
-};
-
-// ввод по кнопке enter
-
-mainForm.addEventListener("keyup", (e) => {
-  if (e.code === "Enter") {
-    buttonElement.click();
-    nameInputElement.value = "";
-    textInputElement.value = "";
-  }
-});
-
-//удаление последнего комментария
-/* const deleteComment = () => {
-  deleteButtonElement.addEventListener("click", () => {
-    const elem = document.getElementById("list").lastChild;
-    elem.parentNode.removeChild(elem);
-  });
-}; */
-
-// ответ на комментарии
-
-const editComment = () => {
-  const comments = document.querySelectorAll(".comment");
-  const textInputElement = document.getElementById("text-input");
-  for (const comment of comments) {
-    comment.addEventListener("click", () => {
-      const textComment = comment.dataset.text;
-      textInputElement.value = textComment;
-    });
-  }
-};
-
-//DOM 2
-
-let comments = [];
-
-//рендер-функция
-
-const renderComments = () => {
-  const commentsHtml =
-    comments &&
-    comments
-      .map((comment, index) => {
-        return `<li data-text = '&gt ${comment.text} \n ${
-          comment.name
-        }' class="comment">
-          <div class="comment-header">
-            <div>${comment.name}</div>
-            <div>${comment.date}</div>
-          </div>
-          <div class="comment-body">
-            <div class="comment-text">
-              ${comment.text}
-            </div>
-          </div>
-          <div class="comment-footer">
-            <div class="likes">
-              <span class="likes-counter">${comment.likes}</span>
-              <button data-index = '${index}' class="${
-          comment.isLiked ? "like-button -active-like" : "like-button"
-        }"></button>
-            </div>
-          </div>
-        </li>`;
-      })
-      .join("");
-  listElement.innerHTML = commentsHtml;
-
-  changeLikesListener();
-  /* deleteComment(); */
-  editComment();
-};
 fetchGetAndRender();
+changeLikesListener();
+editComment();
+validateInput();
 renderComments();
 buttonBlock();
+deleteComment();
+addCommentWithEnter();
 console.log("It works!");
